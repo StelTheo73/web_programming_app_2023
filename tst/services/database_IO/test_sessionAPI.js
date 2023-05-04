76c19da31f0150b737488d5c334f4cd01d2f27be
Theofilou Stylianos <stylianos.theofilou@nokia.com>
2023-05-04 21:20:27 +0300

    Remove tests

--- tst/services/database_IO/test_sessionAPI.js
+++
@@ -1,6 +0,0 @@
-let ses = new SessionAPI();
-let data1 = await ses.getPersonCards("Dias_Pappas_908@hotmail.com")
-let data2 = await ses.getPersonOrders("642d12fd9f0c2c52ba5b81f6", 2021);
-console.log(data1);
-console.log(data2);
-console.log(data2[0].cards)
--- tst/services/translateAPI/test_transalteAPI.js
+++
@@ -1,8 +0,0 @@
-import { TranslatorAPI } from "./services/translateAPI/translateAPI.js";
-let translator = new TranslatorAPI();
-
-for (let i = 0; i < 1000; i ++) {
-    translator.translateText("καλημερα, θελω Παγωτά και μπυρα και κοτοπουλα");
-    translator.translateText("παγωτά");
-    translator.translateText("soublakia kai patates")
-}
