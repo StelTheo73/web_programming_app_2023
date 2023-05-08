e7c09ce48884ec725cd44012397fb9b4d1e2b554
Theofilou Stylianos <stylianos.theofilou@nokia.com>
2023-05-08 14:59:30 +0300

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,4 @@
-echo "Installing requirements..."
+#!/bin/bash
+
+
 pip install -r requirements.txt
-echo "Done"
-
-echo "Starting transalte API server..."
-python -m setup -d true
-echo "Done"
-
-echo "Starting server..."
-node app.mjs
